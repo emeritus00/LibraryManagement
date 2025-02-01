2f678dd1213e4d61ab158c2f8e348edcd2bb7249
Adewale Gbadamosi <adewalegbadamosi@gmail.com>
2025-02-01 19:04:57 -0330

    Add Book class

---
+++ src/main/java/com/keyin/Book.java
@@ -0,0 +1,17 @@
+package com.keyin;
+
+public class Book {
+    private String title;
+    private String author;
+    private boolean isAvailable;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.isAvailable = true;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+}
